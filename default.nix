f2a50da28161b1ed96a7999a5ce570f80392543a
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2026-04-11 11:54:45 +0900

    Update default.nix
    Co-authored-by: rumboon <git@rumboon.eu>

--- default.nix
+++ default.nix
@@ -11,10 +11,16 @@
 
 final: prev: {
   kdePackages = prev.kdePackages.overrideScope (kfinal: kprev: {
-    dolphin-wrapped = prev.writeShellScriptBin "dolphin" ''
-              export XDG_CONFIG_DIRS="${prev.libsForQt5.kservice}/etc/xdg:$XDG_CONFIG_DIRS"
-              ${kprev.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.kservice}/etc/xdg/menus/applications.menu
-              exec ${kprev.dolphin}/bin/dolphin
-    '';
+    dolphin = prev.symlinkJoin {
+      name = "dolphin-wrapped";
+      paths = [ kprev.dolphin ];
+      nativeBuildInputs = [ prev.makeWrapper ];
+      postBuild = ''
+        rm $out/bin/dolphin
+        makeWrapper ${kprev.dolphin}/bin/dolphin $out/bin/dolphin \
+          --set XDG_CONFIG_DIRS "${prev.libsForQt5.kservice}/etc/xdg:$XDG_CONFIG_DIRS" \
+          --run "${kprev.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.kservice}/etc/xdg/menus/applications.menu"
+      '';
+    };
   });
 }
